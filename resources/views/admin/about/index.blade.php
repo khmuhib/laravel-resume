fc24b3e53cb620017163aa4b27c3e7a84aa62022
khmuhib <khmuhib2013@gmail.com>
2022-08-01 18:02:08 +0600

    table now responsive

--- resources/views/admin/about/index.blade.php
+++ resources/views/admin/about/index.blade.php
@@ -20,51 +20,54 @@
                     href="{{ route('admin.about.create') }}">Add About Data</a></h1>
         </div>
         <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Subtitle</th>
-                        <th scope="col">Section Description</th>
-                        <th scope="col">Birth date</th>
-                        <th scope="col">Website Link</th>
-                        <th scope="col">Degree</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">City</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Freelance</th>
-                        <th scope="col">About Description</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">asdasdsa</th>
-                        <td>dasda</td>
-                        <td>dasda</td>
-                        <td>adsdad</td>
-                        <td>dasdsa</td>
-                        <td>dsadas</td>
-                        <td>dsadas</td>
-                        <td>dsadas</td>
-                        <td>dsadas</td>
-                        <td>dsadas</td>
-                        <td>
-                            <img src="" alt="dasda" height="50" width="50">
-                        </td>
-                        <td>
-                            <img src="" alt="dasda" height="50" width="50">
-                        </td>
-                        <td>
-                            <div class="col">
-                                <a href="" class="btn btn-primary">Edit</a>
-                                <a href="" class="btn btn-danger">Delete</a>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Subtitle</th>
+                            <th scope="col">Section Description</th>
+                            <th scope="col">Birth date</th>
+                            <th scope="col">Website Link</th>
+                            <th scope="col">Degree</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">City</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Freelance</th>
+                            <th scope="col">About Description</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">asdasdsa</th>
+                            <td>dasda</td>
+                            <td>dasda</td>
+                            <td>adsdad</td>
+                            <td>dasdsa</td>
+                            <td>dsadas</td>
+                            <td>dsadas</td>
+                            <td>dsadas</td>
+                            <td>dsadas</td>
+                            <td>dsadas</td>
+                            <td>
+                                <img src="" alt="dasda" height="50" width="50">
+                            </td>
+                            <td>
+                                <img src="" alt="dasda" height="50" width="50">
+                            </td>
+                            <td>
+                                <div class="col">
+                                    <a href="" class="btn btn-primary">Edit</a>
+                                    <a href="" class="btn btn-danger">Delete</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
 
         </div>
     </div>
